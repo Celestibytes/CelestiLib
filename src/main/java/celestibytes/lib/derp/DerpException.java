7eb3119ef599dc8fe5645063180d5ba58571f6fa
PizzAna <anttikiviyo@gmail.com>
2014-09-04 15:17:54 +0300

    Change licenses

---
+++ src/main/java/celestibytes/lib/derp/DerpException.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2014 PizzAna
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package celestibytes.lib.derp;
+
+/**
+ * An {@link Exception} that should be thrown whenever derps happen.
+ * 
+ * @author PizzAna
+ *
+ */
+public class DerpException extends Exception
+{
+    /**
+     * The serial version UID.
+     */
+    private static final long serialVersionUID = 3418533804520873407L;
+    
+    /**
+     * Constructs a new {@link DerpException}.
+     */
+    public DerpException()
+    {
+        super();
+    }
+
+    /**
+     * Constructs a new {@link DerpException}.
+     *
+     * @param message
+     *         the message.
+     */
+    public DerpException(String message)
+    {
+        super(message);
+    }
+
+    /**
+     * Constructs a new {@link DerpException}.
+     *
+     * @param message
+     *         the message.
+     * @param cause
+     *         the cause.
+     */
+    public DerpException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new {@link DerpException}.
+     *
+     * @param cause
+     *         the cause.
+     */
+    public DerpException(Throwable cause)
+    {
+        super(cause);
+    }
+}
