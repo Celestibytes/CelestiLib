da1b086de83bf9c0d76d5d078e465a71f9f415ec
OkkapeL <lepakko683@gmail.com>
2014-12-23 22:44:50 +0200

    Remotely Readable value

---
+++ src/main/java/celestibytes/lib/util/IRRValue.java
@@ -0,0 +1,10 @@
+package celestibytes.lib.util;
+
+/** "Remotely Readable" value */
+public interface IRRValue {
+	
+	/** Returns value / max value */
+	public float getRelt();
+	
+	public String getValueTitle();
+}
